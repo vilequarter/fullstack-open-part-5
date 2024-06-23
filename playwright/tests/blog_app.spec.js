4692774625c2ff937790ffa619a4d0fe7804c929
vilequarter <daparady@gmail.com>
2024-06-23 13:55:07 +0300

    exercise 5.19

--- playwright/tests/blog_app.spec.js
+++ playwright/tests/blog_app.spec.js
@@ -1,4 +1,5 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith, createBlog } = require('./helper')
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
@@ -34,6 +35,53 @@ describe('Blog app', () => {
       await page.locator('input[name="Password"]').fill('failing password')
       await page.getByRole('button', { name: "login" }).click()
       await expect(page.getByText('Invalid username or password')).toBeVisible()
+    })
+  })
+
+  describe('When logged in', () => {
+    beforeEach(async ({ page }) => {
+      await loginWith(page, 'Test', 'namtseT .rM')
+    })
+    
+    test('a new blog can be created', async ({ page }) => {
+      const newBlog = {
+        title: "NewBlogTest",
+        author: "The Testerrrr",
+        url: "www.testblog.blog"
+      }
+      await createBlog(page, newBlog)
+      await expect(page.getByText(`Blog ${newBlog.title} by ${newBlog.author} created`)).toBeVisible()
+      await expect(page.getByText(`${newBlog.title} ${newBlog.author}`)).toBeVisible()
+      await expect(page.getByRole('button', { name: "View" })).toBeVisible()
+    })
+
+    describe('With blogs created', () => {
+      beforeEach(async ({ page }) => {
+        const newBlog = {
+          title: "NewBlogTest",
+          author: "The Testerrrr",
+          url: "www.testblog.blog"
+        }
+        await createBlog(page, newBlog)
+      })
+
+      test('a blog can be liked', async ({ page }) => {
+        
+      })
+  
+      test('only a user who created a blog can see the delete button', async ({ page }) => {
+  
+      })
+  
+      test('user who created a blog can delete it' , async ({ page }) => {
+  
+      })
+    })
+  })
+
+  describe('Blog sorting', () => {
+    test('blog list is sorted by descending likes', async ({ page }) => {
+
     })
   })
 })
---
+++ playwright/tests/helper.js
@@ -0,0 +1,15 @@
+const loginWith = async (page, username, password) => {
+  await page.locator('input[name="Username"]').fill(username)
+  await page.locator('input[name="Password"]').fill(password)
+  await page.getByRole('button', { name: "login" }).click()
+}
+
+const createBlog = async (page, blog) => {
+  await page.getByRole('button', { name: 'add blog' }).click()
+  await page.locator('input[name="Title"]').fill(blog.title)
+  await page.locator('input[name="Author"]').fill(blog.author)
+  await page.locator('input[name="Url"]').fill(blog.url)
+  await page.getByRole('button', { name: "Create" }).click()
+}
+
+export { loginWith, createBlog }
